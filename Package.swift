7abb2fb4124c33d675a0145f8eb979316c7a4296
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-10-14 14:00:51 -0700

    Beta AcousticMobilePush build: 3.10.2
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - Update plugins directory.
    - Fix an issue with Swift Packages for release not having correct checksum and manifest.
    
    Improvements:
    
    - Update to minimal of 13.0 and against latest version using xcode 16. Note, you will need to update all the plugins, because
      of changes of iOS 13.0.
    
    Our environment for this release:
    
    - Xcode 16.0
    - MacOS 15.0
    - iOS 13.x to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    	- macos
    		- arm64
    		- x86_64-maccatalyst
    		- x86_64-simulator
    
    Known Issues
    
    - AcousticMobilePushWatch.xcframework is no longer supported and not updated.

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "AcousticMobilePushNotification",
-            url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.10.1/AcousticMobilePushNotification_XCFramework_Debug.zip",
-            checksum: "6a1337537f1826b7794f3942a3612fd9964c296d2c44a9c7c87b4eafe8fe3e62"),
+            url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.10.2/AcousticMobilePushNotification_XCFramework_Debug.zip",
+            checksum: "b445104a68413e7ba4d7f46dbd31e74bef9f80d905881bbcdf9b959bdbbc1480"),
     ]
 )
